375a9a1be0d088aecd65d701ead633d5e186ae64
sean <sean.brady@stylearcade.com.au>
2023-02-07 08:35:28 +1100

    Improvements to layout

--- src/components/SidebarNav.tsx
+++ src/components/SidebarNav.tsx
@@ -10,27 +10,36 @@ export interface SidebarNav {}
 
 export const SidebarNav: React.FC<SidebarNav> = (props) => {
   const ThemeIcon = () => {
-  const [darkTheme, setDarkTheme] = useDarkMode();
-  const handleMode = () => { setDarkTheme(!darkTheme) };
-  return (
-    <span onClick={handleMode}>
-      {darkTheme ? (
-        <FaSun size='24' className='top-navigation-icon' />
-      ) : (
-        <FaMoon size='24' className='top-navigation-icon' />
-      )}
-    </span>
-  );
-};
+    const [darkTheme, setDarkTheme] = useDarkMode();
+    const handleMode = () => { setDarkTheme(!darkTheme) };
+    return (
+      <span onClick={handleMode}>
+        {darkTheme ? (
+          <FaSun size='24' className='top-navigation-icon' />
+        ) : (
+          <FaMoon size='24' className='top-navigation-icon' />
+        )}
+      </span>
+    );
+  };
+
+  const NavItem = (props: React.ComponentProps<typeof Link>) => {
+    return (
+      <Link {...props} className='w-full p-4 rounded-xl hover:bg-grey-300 dark:hover:bg-grey-700 text-lg font-bold mb-4' />
+    )
+  }
 
   return (
-    <nav className='h-screen w-64 flex flex-col bg-white text-black dark:bg-grey-900 dark:text-white shadow-lg'>
-      <Link href='/'>
+    <nav className='h-screen w-64 flex flex-col items-center 
+      text-justify bg-grey-100 dark:bg-grey-900 shadow-lg p-2
+      '
+    >
+      <NavItem href='/'>
         Home
-      </Link>
-      <Link href='/workouts'>
+      </NavItem>
+      <NavItem href='/workouts'>
         Workouts
-      </Link>
+      </NavItem>
       <ThemeIcon />
     </nav>
   )
